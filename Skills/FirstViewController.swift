c13aebc188ea78e6d4251436b25690294607426c
Pawel Pela <paulpela@gmail.com>
2018-11-29 00:55:42 +0100

    Initial Commit

---
+++ Skills/FirstViewController.swift
@@ -0,0 +1,20 @@
+//
+//  FirstViewController.swift
+//  Skills
+//
+//  Created by Paweł Pela on 29/11/2018.
+//  Copyright © 2018 Paweł Pela. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
---
+++ Skills/SecondViewController.swift
@@ -0,0 +1,20 @@
+//
+//  SecondViewController.swift
+//  Skills
+//
+//  Created by Paweł Pela on 29/11/2018.
+//  Copyright © 2018 Paweł Pela. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
