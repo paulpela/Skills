45ed4886673723e56b22657c6ab1d2d19bf12dc8
Pawel Pela <paulpela@gmail.com>
2018-11-29 03:57:13 +0100

    new data and level counting

--- Skills/Skill/Skill.swift
+++ Skills/Skill/Skill.swift
@@ -41,16 +41,16 @@ struct Skill: Codable {
         var colors: (fg: UIColor, bg: UIColor) {
             switch self {
             case .noSkill: return (fg: .white, bg: .darkGray)
-            case .novice: return (fg: .white, bg: .red)
-            case .beginner: return (fg: .white, bg: .red)
-            case .competent: return (fg: .white, bg: .orange)
-            case .skilled: return (fg: .white, bg: .orange)
-            case .proficient: return (fg: .black, bg: .yellow)
-            case .adept: return (fg: .black, bg: .yellow)
-            case .expert: return (fg: .white, bg: .green)
-            case .accomplished: return (fg: .white, bg: .green)
-            case .master: return (fg: .white, bg: .blue)
-            case .legendary: return (fg: .white, bg: .blue)
+            case .novice: return (fg: .white, bg: #colorLiteral(red: 0.3176470697, green: 0.07450980693, blue: 0.02745098062, alpha: 1))
+            case .beginner: return (fg: .white, bg: #colorLiteral(red: 0.3176470697, green: 0.07450980693, blue: 0.02745098062, alpha: 1))
+            case .competent: return (fg: .white, bg: #colorLiteral(red: 0.8527129709, green: 0.3239575046, blue: 0.2078772844, alpha: 1))
+            case .skilled: return (fg: .white, bg: #colorLiteral(red: 0.8527129709, green: 0.3239575046, blue: 0.2078772844, alpha: 1))
+            case .proficient: return (fg: .white, bg: #colorLiteral(red: 0.8540754008, green: 0.5632280909, blue: 0.115415594, alpha: 1))
+            case .adept: return (fg: .white, bg: #colorLiteral(red: 0.8540754008, green: 0.5632280909, blue: 0.115415594, alpha: 1))
+            case .expert: return (fg: .white, bg: #colorLiteral(red: 0.1960784346, green: 0.3411764801, blue: 0.1019607857, alpha: 1))
+            case .accomplished: return (fg: .white, bg: #colorLiteral(red: 0.1960784346, green: 0.3411764801, blue: 0.1019607857, alpha: 1))
+            case .master: return (fg: .white, bg: #colorLiteral(red: 0.1019607857, green: 0.2784313858, blue: 0.400000006, alpha: 1))
+            case .legendary: return (fg: .white, bg: #colorLiteral(red: 0.1019607857, green: 0.2784313858, blue: 0.400000006, alpha: 1))
             }
         }
     }
@@ -73,7 +73,9 @@ struct Skill: Codable {
                     result + Double(skill.level?.rawValue ?? 0)
                 })
                 
-                let count = Double(items?.count ?? 0)
+                let count = Double(items?.filter({ (skill) -> Bool in
+                    skill.level != nil
+                }).count ?? 0)
                 
                 if count == 0.0 {
                     return _level
--- Skills/Skill/SkillTableViewCell.swift
+++ Skills/Skill/SkillTableViewCell.swift
@@ -31,8 +31,10 @@ class SkillTableViewCell: UITableViewCell {
             
             if skill.items != nil {
                 accessoryType = .disclosureIndicator
+                isUserInteractionEnabled = true
             } else {
                 accessoryType = .none
+                isUserInteractionEnabled = false
             }
         }
     }
