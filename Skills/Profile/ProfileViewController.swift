88e83356ab1d644571c1c18d5b77d0f4d2b4a679
Pawel Pela <paulpela@gmail.com>
2018-11-29 21:30:46 +0100

    added per tab skill display to profile

--- Skills/Profile/ProfileViewController.swift
+++ Skills/Profile/ProfileViewController.swift
@@ -12,11 +12,19 @@ class ProfileViewController: UIViewController {
     @IBOutlet weak var levelBackground: UIView!
     @IBOutlet weak var levelLabel: UILabel!
     
+    @IBOutlet var tabBackgrounds: [UIView]!
+    @IBOutlet var levelLabels: [UILabel]!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
         levelBackground.layer.cornerRadius = 4
         levelBackground.layer.masksToBounds = true
+        
+        for tabBackground in tabBackgrounds {
+            tabBackground.layer.cornerRadius = 4
+            tabBackground.layer.masksToBounds = true
+        }
     }
     
     override func viewWillAppear(_ animated: Bool) {
@@ -24,10 +32,13 @@ class ProfileViewController: UIViewController {
         
         levelBackground.backgroundColor = AppDelegate.rootSkill?.level?.colors.bg ?? .darkGray
         levelLabel.text = AppDelegate.rootSkill?.level?.string ?? "no skill"
+        
+        if let tabSkills = AppDelegate.rootSkill?.items {
+            for (index, tab) in tabSkills.enumerated() {
+                tabBackgrounds[index].backgroundColor = tab.level?.colors.bg ?? .darkGray
+                levelLabels[index].text = tab.level?.string ?? "no skill"
+            }
+        }
     }
 
-    @IBAction func closeButtonPressed(_ sender: Any) {
-        //self.dismiss(animated: true, completion: nil)
-    }
-    
 }
