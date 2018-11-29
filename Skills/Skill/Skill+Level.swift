e597727d67ae4cd857ab7cbb60e414015c467e82
Pawel Pela <paulpela@gmail.com>
2018-11-29 05:43:43 +0100

    skill level colours is represented as hue

--- Skills/Skill/Skill+Level.swift
+++ Skills/Skill/Skill+Level.swift
@@ -9,7 +9,7 @@
 import UIKit
 
 extension Skill {
-    enum Level: Int, Codable {
+    enum Level: Int, Codable, CaseIterable {
         case noSkill        = 0
         case novice         = 1
         case beginner       = 2
@@ -39,34 +39,46 @@ extension Skill {
         }
         
         var colors: (fg: UIColor, bg: UIColor) {
-            switch self {
-            case .noSkill: return (fg: .white, bg: .darkGray)
-            case .novice: return (fg: .white, bg: #colorLiteral(red: 0.3176470697, green: 0.07450980693, blue: 0.02745098062, alpha: 1))
-            case .beginner: return (fg: .white, bg: #colorLiteral(red: 0.3176470697, green: 0.07450980693, blue: 0.02745098062, alpha: 1))
-            case .competent: return (fg: .white, bg: #colorLiteral(red: 0.8527129709, green: 0.3239575046, blue: 0.2078772844, alpha: 1))
-            case .skilled: return (fg: .white, bg: #colorLiteral(red: 0.8527129709, green: 0.3239575046, blue: 0.2078772844, alpha: 1))
-            case .proficient: return (fg: .white, bg: #colorLiteral(red: 0.8540754008, green: 0.5632280909, blue: 0.115415594, alpha: 1))
-            case .adept: return (fg: .white, bg: #colorLiteral(red: 0.8540754008, green: 0.5632280909, blue: 0.115415594, alpha: 1))
-            case .expert: return (fg: .white, bg: #colorLiteral(red: 0.1960784346, green: 0.3411764801, blue: 0.1019607857, alpha: 1))
-            case .accomplished: return (fg: .white, bg: #colorLiteral(red: 0.1960784346, green: 0.3411764801, blue: 0.1019607857, alpha: 1))
-            case .master: return (fg: .white, bg: #colorLiteral(red: 0.1019607857, green: 0.2784313858, blue: 0.400000006, alpha: 1))
-            case .legendary: return (fg: .white, bg: #colorLiteral(red: 0.1019607857, green: 0.2784313858, blue: 0.400000006, alpha: 1))
-            }
+            let hueMax: CGFloat = 220.0
+            let degreesPerLevel = hueMax / CGFloat(Level.allCases.count) * (1.0 / 360.0)
+            
+            return (fg: .white, bg: UIColor(
+                hue: CGFloat(self.rawValue) * degreesPerLevel,
+                saturation: 0.9,
+                brightness: 0.7,
+                alpha: 1.0
+            ))
         }
-        
-        var description: String {
+
+        var requirements: [String] {
             switch self {
-            case .noSkill: return "You have no skill in this domain."
-            case .novice: return "You have started acquiring basic knowledge in this domain."
-            case .beginner: return "You have acquired some basic knowledge in this domain."
-            case .competent: return "You are beginning to play with your knowledge in this domain."
-            case .skilled: return "You have played with your knowledge in this domain for some time."
-            case .proficient: return "You have created basic products based on the knowledge of this domain."
-            case .adept: return "You have created more advanced products based on the knowledge of this domain."
-            case .expert: return "You are beginning to use the knowledge from this domain to creatively resolve problems."
-            case .accomplished: return "You are regularly using the knowledge from this domain to creatively resolve problems."
-            case .master: return "You have invented some solutions in this domain."
-            case .legendary: return "You have invented the solution in this domain."
+            case .noSkill: return
+                [
+                    "You have no skill in this domain."
+                ]
+            case .novice: return
+                [
+                    "You have started acquiring basic knowledge in this domain.",
+                    "You know this exists, but would not be able to write any code on paper.",
+                    "You may have created a project following a tutorial."
+                ]
+            case .beginner: return
+                [
+                    "You have acquired some basic knowledge in this domain.",
+                    "You have used this knowledge in some simple projects of your own."
+                ]
+            case .competent: return [ "You are beginning to play with your knowledge in this domain." ]
+            case .skilled: return [ "You have played with your knowledge in this domain for some time." ]
+            case .proficient: return
+                [
+                    "You have created basic products based on the knowledge of this domain.",
+                    "Other people are using your products you have created using knowledge in this domain."
+                ]
+            case .adept: return [ "You have created more advanced products based on the knowledge of this domain." ]
+            case .expert: return [ "You are beginning to use the knowledge from this domain to creatively resolve problems." ]
+            case .accomplished: return [ "You are regularly using the knowledge from this domain to creatively resolve problems." ]
+            case .master: return [ "You have invented some solutions in this domain." ]
+            case .legendary: return [ "You have invented the solution in this domain." ]
             }
         }
     }
